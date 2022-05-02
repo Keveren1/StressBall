e28f25b7d4b2479bd28adb4e371f7b2c1caa4360
LailaDM <lail0894@edu.easj.dk>
2022-05-02 09:25:04 +0200

    Add project files.

---
+++ StressBall/Model/StressBallData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StressBall
+{
+    public class StressBallData
+    {
+        public string Acceleration { get; set; }
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
