28f7f3ef8f0e84d29f410a7ecd2209d4490ffdec
Asland3 <malthe3@outlook.com>
2022-05-05 13:08:35 +0200

    test

--- UDPServer/Program.cs
+++ UDPServer/Program.cs
@@ -20,15 +20,12 @@ using (HttpClient client = new HttpClient())
             string message = Encoding.UTF8.GetString(data);
         
             Console.WriteLine("Server received: " + message);
+            
 
-            Console.WriteLine(data[1]);
-            
-            
-        
             HttpContent content = new StringContent(message, Encoding.UTF8, "application/json");
             client.PostAsync("https://stressballapi.azurewebsites.net/api/StressBall", content);
             
-            socket.Send(data, data.Length, clientEndPoint); 
+            //socket.Send(data, data.Length, clientEndPoint); 
         }
     }
 }
